01fe39c11727d6c6e453c5140e7d642142fb978e
eitchtee <flaips.2@gmail.com>
2020-07-23 21:24:05 -0300

    Add support for filtering though # anchors

--- javascript/filters.js
+++ javascript/filters.js
@@ -1,15 +1,30 @@
 $('.filter-buttons').removeClass('hiding'); // Show filter is JS is enabled
-    
-    // from https://codepen.io/terf/post/jquery-filter-divs
+var selector_query = window.location.hash.substr(1);
+
+if (selector_query) {
+    if ($('#filter-' + selector_query).length) {
+        filter(selector_query);
+        $('.button.filter').removeClass('active');
+        $('#filter-' + selector_query).addClass('active');
+        $('#filter-' + selector_query).blur();
+    }
+}
+
 var $btns = $('.button.filter').click(function () {
-    if (this.id == 'all') {
+    filter(this.id.replace("filter-", ""));
+
+    $btns.removeClass('active');
+    $(this).addClass('active');
+    $(this).blur();
+})
+
+function filter(category) {
+    if (category.toLowerCase() == 'all' || category.toLowerCase() == 'todos') {
         $('#filterable > div, #filterable > a').fadeIn(450);
     } else {
-        var $el = $('.' + this.id).fadeIn(450);
+        var $el = $('.' + category).fadeIn(450);
         $('#filterable > div, #filterable > a').not($el).hide(0, function() {
             $('#filterable > div, #filterable > a').not($el).attr("style", "display: none !important");
         });
     }
-    $btns.removeClass('active');
-    $(this).addClass('active');
-})
+}
